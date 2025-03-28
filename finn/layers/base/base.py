247dd1d472f1f045fccffa8058cc98c7dd2785fd
AnniekStok <anniek.stokkermans@gmail.com>
2025-03-28 09:14:43 +0100

    remove redundant (?) clipping plane event call from base.py

--- finn/layers/base/base.py
+++ finn/layers/base/base.py
@@ -1253,7 +1253,6 @@ class Layer(KeymapProvider, MousemapProvider, ABC, metaclass=PostInit):
             plane = ClippingPlane()
             plane.update(new_plane)
             self._experimental_clipping_planes.append(plane)
-        self.events.experimental_clipping_planes()
 
     @property
     def bounding_box(self) -> Overlay:
