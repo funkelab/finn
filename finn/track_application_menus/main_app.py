2b612312855a7573df38f728b60f6d8aee216ef1
AnniekStok <anniek.stokkermans@gmail.com>
2025-04-03 15:10:29 +0200

    Create QSplitter to optionally show the orthogonal views next to the menu widget

--- finn/track_application_menus/main_app.py
+++ finn/track_application_menus/main_app.py
@@ -1,4 +1,7 @@
+from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
+    QPushButton,
+    QSplitter,
     QVBoxLayout,
     QWidget,
 )
@@ -6,6 +9,7 @@ from qtpy.QtWidgets import (
 import finn
 from finn.track_application_menus.menu_widget import MenuWidget
 from finn.track_data_views.views.tree_view.tree_widget import TreeWidget
+from finn.track_data_views.views.view_3d.orthogonal_views import OrthogonalViews
 
 
 class MainApp(QWidget):
@@ -13,13 +17,45 @@ class MainApp(QWidget):
 
     def __init__(self, viewer: finn.Viewer):
         super().__init__()
+        self.viewer = viewer
 
-        menu_widget = MenuWidget(viewer)
+        self.viewer.dims.events.ndim.connect(self.dims_changed)
+
+        self.menu_widget = MenuWidget(viewer)
+        self.orth_views = OrthogonalViews(viewer)
         tree_widget = TreeWidget(viewer)
 
+        self.splitter = QSplitter(Qt.Horizontal)  # Set orientation to horizontal
+        self.splitter.addWidget(self.orth_views)  # Add the orthogonal views
+        self.splitter.addWidget(self.menu_widget)  # Add the menu widget
+        self.splitter.setSizes([0, 200])
         viewer.window.add_dock_widget(tree_widget, area="bottom", name="Tree View")
 
         layout = QVBoxLayout()
-        layout.addWidget(menu_widget)
+
+        self.collapse_btn = QPushButton("Show/hide orthogonal views")
+        self.collapse_btn.clicked.connect(self.toggle_orth_views)
+        if self.viewer.dims.ndim < 3:
+            self.collapse_btn.hide()
+        layout.addWidget(self.collapse_btn)
+        layout.addWidget(self.splitter)
 
         self.setLayout(layout)
+
+    def toggle_orth_views(self):
+        """Show/Hide the orthogonal views depending on their current state"""
+        sizes = self.splitter.sizes()
+        if sizes[0] > 0:
+            self.splitter.setSizes([0, 200])
+        else:
+            self.splitter.setSizes([300, 200])
+
+    def dims_changed(self):
+        """Show/Hide the orthogonal views depending on the amount of dimensions of the viewer"""
+
+        if self.viewer.dims.ndim > 2:
+            self.collapse_btn.show()
+            self.splitter.setSizes([300, 200])
+        else:
+            self.collapse_btn.hide()
+            self.splitter.setSizes([0, 200])
--- finn/track_data_views/views/view_3d/orthogonal_views.py
+++ finn/track_data_views/views/view_3d/orthogonal_views.py
@@ -1,5 +1,4 @@
-from psygnal import Signal
-from qtpy.QtWidgets import QLabel, QVBoxLayout, QWidget
+from qtpy.QtWidgets import QVBoxLayout, QWidget
 
 import finn
 from finn.track_data_views.views.view_3d.multiple_view_widget import (
@@ -25,55 +24,3 @@ class OrthogonalViews(QWidget):
         layout.addWidget(self.cross_widget)
 
         self.setLayout(layout)
-
-    def update(self):
-        self.multiple_viewer_widget.update()
-
-
-class View3D(QWidget):
-    """Widget to combine multiple views and cross widget together."""
-
-    update_tab = Signal()
-
-    def __init__(
-        self,
-        viewer: finn.Viewer,
-    ):
-        super().__init__()
-
-        self.viewer = viewer
-        self.viewer.dims.events.ndisplay.connect(self.display_changed)
-        self.viewer.dims.events.ndim.connect(self.display_changed)
-
-        self.orth_views = OrthogonalViews(viewer)
-        widget_2d_layout = QVBoxLayout()
-        widget_2d_layout.addWidget(QLabel("Viewer has less than 3 dimensions"))
-        self.widget_2d = QWidget()
-        self.widget_2d.setLayout(widget_2d_layout)
-
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-
-        # Add both widgets to the layout but hide them initially
-        self.layout().addWidget(self.orth_views)
-        self.layout().addWidget(self.widget_2d)
-
-        self.orth_views.hide()
-        self.widget_2d.hide()
-
-        # Show the correct widget
-        self.display_changed()
-
-    def display_changed(self, event=None):
-        """Switch between OrthogonalViews 2D widget based on how many dimensions the viewer has."""
-        # Hide both widgets
-        self.orth_views.hide()
-        self.widget_2d.hide()
-
-        # Show the appropriate widget based on ndisplay
-        if self.viewer.dims.ndim >= 3:
-            self.orth_views.show()
-            self.orth_views.update()
-            self.update_tab.emit()
-        else:
-            self.widget_2d.show()
